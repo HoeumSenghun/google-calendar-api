c1338d81ba739e6fc44648262d39d368c71c470d
HoeumSenghun <hoeumsenghun369@gmail.com>
2025-06-30 23:55:50 +0700

    style

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 
 export default function Home() {
   return (
-    <div className="text-3xl underline">Test Style Tailwindcss!</div>
+    <div className="text-3xl underline">Tailwindcss style</div>
   );
 }
